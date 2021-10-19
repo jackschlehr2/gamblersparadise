a3dbd34e096950889b83bb495383b8047e8aff68
John Schlehr <jschlehr@nd.edu>
2021-10-19 14:58:02 -0400

    sign in owrks

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,6 @@ def login_required(f):
             return f(*args, **kwargs)
         else:
             flash("You need to login first")
-            return redirect(url_for('login_page'))
+            return redirect(url_for( 'login') )
 
     return wrap
--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, session
 # import MySQLdb
 from flask_mysqldb import MySQL
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 mysql = MySQL()
 
@@ -13,6 +13,7 @@ app.config['MYSQL_USER'] = 'jschlehr'
 app.config['MYSQL_PASSWORD'] = 'notr3dam3'
 app.config['MYSQL_DB'] = 'jschlehr'
 app.config['MYSQL_HOST'] = 'localhost'
+app.secret_key = "supersecretkey321"
 mysql.init_app(app)
 
 @app.route("/")
@@ -36,14 +37,40 @@ def signUp():
         curr.execute("INSERT INTO users (user_username, user_password, user_email) VALUES (%s, %s, %s)", 
                                                 ( _name, _hashed_password, _email ) )
         conn.commit()
+        session['logged_in'] = True
         return json.dumps({'message':'usermade'})
     else:
         return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method=='GET':
+        return render_template('login.html')
+    elif request.method=='POST':
+        _username = request.form['inputName']
+        _password = request.form['inputPassword']
+        if _username and _password:
+            conn = mysql.connection
+            curr = conn.cursor()
+            curr.execute("SELECT * FROM users WHERE user_username = (%s) or user_email = (%s)", 
+                                                    ( _username, _username ) )
+            
+            data = curr.fetchall()
+            if not data:
+                return json.dumps( {'fail':'fail'})
+            # TODO: case where query returns more than one result
+            # refactor to make more robust
+            query_password = data[0][3]
+
+            # TODO fix so that the returns are better
+            if check_password_hash(query_password, _password):
+                session['logged_in'] = True
+                return redirect( url_for( 'account') )
+            return json.dumps( {'fail': "fail"})
+
+
+
 
 
 if __name__== "__main__":
