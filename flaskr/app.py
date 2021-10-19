4122d5ae6e5db2b5b8309c06b7e8c7c83804ac26
John Schlehr <jschlehr@nd.edu>
2021-10-19 12:38:20 -0400

    allows connection

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, json
 # import MySQLdb
 from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash
+
+mysql = MySQL()
 
 app = Flask(__name__)
-mysql = MySQL()
  
 # MySQL configurations
-# app.config['MYSQL_DATABASE_USER'] = 'jschlehr'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'notr3dam3'
-# app.config['MYSQL_DATABASE_DB'] = 'BucketList'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
+ 
+app.config['MYSQL_DATABASE_USER'] = 'jschlehr'
+app.config['MYSQL_PASSWORD'] = 'notr3dam3'
+app.config['MYSQL_DATABASE_DB'] = 'jschlehr'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def main():
@@ -28,7 +31,11 @@ def signUp():
     _password = request.form['inputPassword']
     print(_name, _email, _password )
     if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
+        curr = mysql.connection.cursor()
+        _hashed_password = generate_password_hash(_password)
+        # curr.execute("INSERT INTO users ")
+        return json.dumps({'message':'usermade'})
+
     else:
         return json.dumps({'html':'<span>Enter the required fields</span>'})
 
