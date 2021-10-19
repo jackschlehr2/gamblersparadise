160561179e618d5a95303979a93365226dec0f21
John Schlehr <jschlehr@nd.edu>
2021-10-19 11:49:16 -0400

    waiting on flask_mysqldb

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, json
+# import MySQLdb
+from flask_mysqldb import MySQL
 
+app = Flask(__name__)
+mysql = MySQL()
+ 
+# MySQL configurations
+# app.config['MYSQL_DATABASE_USER'] = 'jschlehr'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'notr3dam3'
+# app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# mysql.init_app(app)
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
-@app.route('/sign-up')
-def signup():
+
+@app.route('/sign-up-page')
+def signUpPage():
     return render_template('signup.html')
+
+@app.route('/sign-up', methods=['POST'])
+def signUp():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+    print(_name, _email, _password )
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 @app.route('/login')
 def login():
